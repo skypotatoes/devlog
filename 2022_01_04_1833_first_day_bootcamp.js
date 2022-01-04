f52282cebd95f2b2cede76b04d679c5381e78937
skypotatoes <ronan.dodds@gmail.com>
2022-01-04 18:45:34 +0000

    first day at bootcamp

---
+++ 2022_01_04_1833_first_day_bootcamp.js
@@ -0,0 +1,19 @@
+// Today I started the Northcoders bootcamp. It was mainly going over git and github's basic functions,
+// which I already have a pretty good grasp of because I've been using it to update this repo.
+
+// We were given some tasks at the end of the day, which can be seen here: https://github.com/skypotatoes/word-play
+// These were some exercises to get us used to the terminal command line, so it is good to have gained a bit more
+// experience in how it works. The hardest bit was using grep to search for all words with 3 or more vowels in a
+// list and then output those words to a file. For a while, I was trying a regex match which I was sure was correct
+// but couldn't get it to work. In the end, it turned out I needed to use egrep rather than grep to get it to work
+// with regex. Good to know.
+
+// We were also sent these exercises to fork and clone: https://github.com/skypotatoes/remote-intro-week
+// I don't think I was meant start them yet but once I got looking at them, I couldn't resist solving the array methods
+// exercises. They are very simple tasks, especially compared to some of the kata I have been looking at over the last
+// few weeks, however, by making them simple and requiring a solution using only array methods, it has helped me to learn
+// the correct use of things like arr.filter() and arr.map(), which I have used before but was not as comfortable with as
+// things like for loops. Had I not been restricted to using array methods, I think I would have probably tried to solve
+// some of these with for loops. So after doing half a dozen or so of these, I feel I have a much better grasp of them.
+// Anyway, I am going to refrain from jumping ahead and take a break from coding for the night. It's not even 7pm and I
+// feel knackered. Not used to being up in the morning these days!
